f27a610b9f14796dd4d0cf30485f1c9a01d08285
Sarah Choung <sarahjchoung@gmail.com>
2021-01-30 14:22:56 -0800

    circles dynamically fill

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -7,7 +7,10 @@ class Carousel {
     this.renderArrow = this.renderArrow.bind(this);
     this.renderCarousel = this.renderCarousel.bind(this);
     this.renderImages = this.renderImages.bind(this);
+    this.renderCircles = this.renderCircles.bind(this);
+    this.fillCircle = this.fillCircle.bind(this);
     this.carouselImages = null;
+    this.circles = null;
 
   }
 
@@ -17,6 +20,7 @@ class Carousel {
     this.carouselImages.forEach(item => {
       item.style.transform = `translateX(-${this.currentIndex * 100}%)`;
     });
+    this.fillCircle();
   }
 
   goRight() {
@@ -25,6 +29,7 @@ class Carousel {
     this.carouselImages.forEach(item => {
       item.style.transform = `translateX(-${this.currentIndex * 100}%)`;
     });
+    this.fillCircle();
   }
 
   renderArrow(iconClass, side, onClickFunction) {
@@ -46,11 +51,33 @@ class Carousel {
     });
   }
 
+  renderCircles(container, images) {
+    const circleContainer = document.createElement('div');
+    circleContainer.className = 'circle-container';
+    images.forEach((imageLink, index) => {
+      const circle = document.createElement('div');
+      circle.className = index === this.currentIndex ? 'circle active' : 'circle';
+      circleContainer.appendChild(circle);
+    });
+    container.appendChild(circleContainer);
+  }
+
+  fillCircle() {
+    this.circles.forEach((item, index) => {
+      if (index === this.currentIndex) {
+        item.className = 'circle active';
+      } else {
+        item.className = 'circle';
+      }
+    });
+  }
+
   renderCarousel() {
     const carouselContainer = document.createElement('div');
     carouselContainer.className = 'carousel-container';
 
     this.renderImages(carouselContainer, this.images);
+    this.renderCircles(carouselContainer, this.images);
 
     const leftArrow = this.renderArrow('fas fa-2x fa-chevron-left', 'left-arrow', this.goLeft);
     const rightArrow = this.renderArrow('fas fa-2x fa-chevron-right', 'right-arrow', this.goRight);
@@ -59,6 +86,9 @@ class Carousel {
     document.querySelector('body').appendChild(carouselContainer);
     const carouselImages = document.querySelectorAll('.carousel-image');
     this.carouselImages = carouselImages;
+
+    const circles = document.querySelectorAll('.circle');
+    this.circles = circles;
   }
 }
 
